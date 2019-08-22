5bccf3f1b8a53ef5a5af940e93705586ea8fb1d7
Matthew <mfarmer5102@gmail.com>
2019-08-22 19:35:39 -0400

    Implementing rendering of cards for patients in all-patient-search

--- public/js/patientSearch.js
+++ public/js/patientSearch.js
@@ -1,7 +1,21 @@
 $.ajax({
-  //url: "http://localhost:5000/api/patientsearch/all",
   url: "https://patientplus.herokuapp.com/api/patientsearch/all",
   method: "GET"
 }).then(function(response) {
   console.log(response);
+  for (i = 0; i < response.length; i++) {
+    $("#cardContainer").append(
+      `
+      <div class="card">
+        <div class="card-header">
+        ${response[i].lastName}, ${response[i].firstName}
+        </div>
+        <div class="card-body">
+        ${response[i].ssn}
+        ${response[i].birthDate}
+        </div>
+      </div>
+    `
+    );
+  }
 });
