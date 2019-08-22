02f08d3ec192b8080edc1f7bb2dff56c309750ee
Matthew <mfarmer5102@gmail.com>
2019-08-22 19:45:08 -0400

    Reformatting how cards are constructed by jquery

--- public/js/patientSearch.js
+++ public/js/patientSearch.js
@@ -4,18 +4,18 @@ $.ajax({
 }).then(function(response) {
   console.log(response);
   for (i = 0; i < response.length; i++) {
-    $("#cardContainer").append(
-      `
-      <div class="card">
-        <div class="card-header">
-        ${response[i].lastName}, ${response[i].firstName}
-        </div>
-        <div class="card-body">
-        ${response[i].ssn}
-        ${response[i].birthDate}
-        </div>
-      </div>
-    `
-    );
+    //Define define and fill card header
+    var header = $("<div class='card-header'></div>");
+    header.append(response[i].lastName + ", " + response[i].firstName);
+
+    //Define and fillcard body
+    var body = $("<div class='card-body'></div>");
+    body.append(response[i].ssn);
+    body.append(response[i].birthDate);
+
+    //Define and fill card
+    var card = $("<div class='card'></div>");
+    card.append(header);
+    card.append(body);
   }
 });
