c041d6801c056e6c818eb54bed4cdb61b940f2cc
Matthew <mfarmer5102@gmail.com>
2019-08-21 23:33:46 -0400

    Added comment above add snapshot route

--- routes/addSnapshot.js
+++ routes/addSnapshot.js
@@ -2,6 +2,7 @@ var db = require("../models");
 console.log(db);
 
 module.exports = function(app) {
+  //Add a new snapshot
   app.post("/api/addsnapshot", function(req, res) {
     db.Snapshots.create(req.body).then(function(result) {
       res.json(result);
