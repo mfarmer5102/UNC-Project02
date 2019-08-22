64304bbde403a75f7bede1205a75eacbdf9a3f7f
Matthew <mfarmer5102@gmail.com>
2019-08-22 19:04:59 -0400

    Adding patientsearch and patientsnapshots files, plus testing ajax call for patientsearch js file

--- public/js/patientSearch.js
+++ public/js/patientSearch.js
@@ -1,1 +1,7 @@
-alert("connected");
+$.ajax({
+  //url: "http://localhost:5000/api/patientsearch/all",
+  url: "https://patientplus.herokuapp.com/api/patientsearch/all",
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+});
