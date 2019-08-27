422484c1d67a585b2efd1f1d6080b7b0bc2a8f17
Matthew <mfarmer5102@gmail.com>
2019-08-26 21:13:32 -0400

    Travis CI still timing out after 11 mins and refusing to pass [skip ci]

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-// Starting the server, syncing our models ------------------------------------/
+// Starting the server, syncing our models -------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(
