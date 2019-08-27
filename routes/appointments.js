e6e93d2527e3d86e8cb489ecb178502ecaaf4c83
Matthew <mfarmer5102@gmail.com>
2019-08-26 22:13:07 -0400

    Travis CI timing out [skip ci]

--- routes/appointments.js
+++ routes/appointments.js
@@ -75,7 +75,7 @@ module.exports = function(app) {
   });
 };
 
-// Define function for sending email to a patient /////////////////////////////////////////////////
+// Define function for sending email to a patient //////////////////////////////////////////////////
 
 function sendEmailToPatient(
   recipientEmail,
